d3136df3c49021420041e931b4f1326230a1d7f8
ianuraggavhane <77688394+Anurag6596@users.noreply.github.com>
2024-12-04 13:17:04 +0530

    created user model and made it required

---
+++ Backend/models/user.model.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+    fullname:{
+        firstname:{
+            type:String,
+            required:true,
+            minlength:[3,"Must be three charecter or long"],
+        },
+        lastname:{
+         type:String,
+            // required:true,
+            minlength:[3,"Must be three charecter or long"],
+        }
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        minlength: [5, "email must be at leat 5 charecters or above"],
+    },
+    password:{
+        type:String,
+        required:true,
+        select:false,
+    },
+
+    socketId: {
+        type: String,
+    }, // this is used fir sharing the live location of the rider to track the pilots location
+
+})
+
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY, { expiresIn: '24hrs'});
+    return token;
+}
+ userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+ }
+
+ userSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+ }
+
+ const userModel = mongoose.model('user', userSchema);
+
+ module.exports = userModel;
