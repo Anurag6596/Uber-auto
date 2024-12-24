fba5f67df20503c404be2c0c6eacf7bc5ef516c5
ianuraggavhane <77688394+Anurag6596@users.noreply.github.com>
2024-12-24 15:49:33 +0530

    created home page UI and functionality with Location search panel

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -1,30 +1,37 @@
 import React from 'react'
 
-const LocationSearchPanel = () => {
+const LocationSearchPanel = (props) => {
+
+
+  // sample array for location
+
+  const locations = [
+    'House no.2146 behind patel lawns saoner, new Gujarkhedi',
+    'House no.2145 behind jaiswal lawn saoner, new Gujarkhedi',
+    'House no.2144 behind manaswi pater plant saoner, new Gujarkhedi',
+    'House no.2143 behind gavhane Residence saoner, new Gujarkhedi',
+    'House no.2142 behind gayatri niwas saoner, new Gujarkhedi',
+  ]
+
+
   return (
     <div>
       {/* this is just a sample data */}
+      {
+        locations.map(function(elem,idx){
+          // eslint-disable-next-line react/jsx-key
+          return <div key={idx} onClick={()=>{
+            props.setVehiclePanel(true)
+            props.setpanelOpen(false)
+          }} className='flex gap-4 border-2 p-3 border-gray-30 active:border-black rounded-xl items-center my-2 justify-start'>
+          <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+          <h4 className='text-medium'>{elem}</h4>
+        </div>
+        })
+      }
 
-      <div className='flex gap-4 items-center my-4 justify-start'>
-        <h2 className='bg-[#eee] h-8 flex items-4enter justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
-        <h4 className='text-medium'>House no.2146 behinf patel lawns saoner, new Gujarkhedi</h4>
-      </div>
-      <div className='flex gap-4 items-center my-4 justify-start'>
-        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
-        <h4 className='text-medium'>House no.2146 behinf patel lawns saoner, new Gujarkhedi</h4>
-      </div>
-      <div className='flex gap-4 items-center my-4 justify-start'>
-        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
-        <h4 className='text-medium'>House no.2146 behinf patel lawns saoner, new Gujarkhedi</h4>
-      </div>
-      <div className='flex gap-4 items-center my-4 justify-start'>
-        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
-        <h4 className='text-medium'>House no.2146 behinf patel lawns saoner, new Gujarkhedi</h4>
-      </div>
-      <div className='flex gap-4 items-center my-4 justify-start'>
-        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
-        <h4 className='text-medium'>House no.2146 behinf patel lawns saoner, new Gujarkhedi</h4>
-      </div>
+
+
     </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,8 +8,10 @@ const Home = () => {
   const [pickUp, setPickUp] = useState("");
   const [destination, setDestination] = useState("");
   const [panelOpen, setpanelOpen] = useState(false);
+  const vehiclePanelRef = useRef(null)
   const panelRef = useRef(null);
   const panelCloseRef = useRef(null);
+  const [vehiclePanelOpen, setVehiclePanelOpen] = useState(false)
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -40,8 +42,22 @@ const Home = () => {
     [panelOpen]
   );
 
+
+  useGSAP(function () {
+    if (vehiclePanelOpen) {
+        gsap.to(vehiclePanelRef.current, {
+            transform: 'translateY(0)'
+        })
+    } else {
+        gsap.to(vehiclePanelRef.current, {
+            transform: 'translateY(100%)'
+        })
+    }
+}, [ vehiclePanelOpen ])
+
+
   return (
-    <div className="h-screen relative">
+    <div className="h-screen relative overflow-hidden">
       <img
         className="w-16 absolute left-5 top-5"
         src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
@@ -55,7 +71,7 @@ const Home = () => {
           src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
           alt=""
         />
-      </div>
+      </div> 
       <div className=" flex flex-col justify-end h-screen absolute top-0 w-full">
         <div className="h-[30%] p-6 bg-white relative">
           <h5
@@ -97,10 +113,42 @@ const Home = () => {
           </form>
         </div>
         <div ref={panelRef} className="bg-white h-0">
-          <LocationSearchPanel />
+          <LocationSearchPanel setpanelOpen={setpanelOpen} setVehiclePanel={setVehiclePanelOpen }  />
         </div>
       </div>
-      <div></div>
+      <div ref={vehiclePanelRef} className="fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-14">
+      <h5 onClick={()=>{
+        setVehiclePanelOpen(false)
+      }} className="p-1 text-center w-[93%] absolute top-3"><i className=" text-3xl text-gray-200 ri-arrow-down-wide-fill"></i></h5>
+        <h2 className="text-2xl font-semibold mb-3">Choose a Vehicle</h2>
+        <div className="flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between">
+            <img className="h-10" src="https://www.pngplay.com/wp-content/uploads/8/Uber-PNG-Photos.png " alt="" />
+            <div className=" ml-2 w-1/2">
+              <h4 className="font-medium text-base">UberGo <span><i className="ri-user-3-fill"></i>4</span></h4>
+              <h5 className="font-normal text-xs text-gray-600">2 mins away</h5>
+              <p className="font-medium text-xs">Affordable, compact rides</p>
+            </div>
+            <h2 className="text-lg font-semibold">₹195.23</h2>
+        </div>
+        <div className="flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between">
+            <img className="h-10" src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png" alt="" />
+            <div className=" ml-2 w-1/2">
+              <h4 className="font-medium text-base">UberMoto <span><i className="ri-user-3-fill"></i></span></h4>
+              <h5 className="font-normal text-xs text-gray-600">2 mins away</h5>
+              <p className="font-medium text-xs">Affordable, MotorCycle rides</p>
+            </div>
+            <h2 className="text-lg font-semibold">₹76.06</h2>
+        </div>
+        <div className="flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between">
+            <img className="h-10" src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png" alt="" />
+            <div className=" ml-2 w-1/2">
+              <h4 className="font-medium text-base">UberAuto <span><i className="ri-user-3-fill"></i>6</span></h4>
+              <h5 className="font-normal text-xs text-gray-600">2 mins away</h5>
+              <p className="font-medium text-xs">Affordable rides</p>
+            </div>
+            <h2 className="text-lg font-semibold">₹80.10</h2>
+        </div>
+      </div>
     </div>
   );
 };
